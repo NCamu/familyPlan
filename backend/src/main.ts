4f20b3e350393a834e1012b0964c9f2f298851ca
Your name <Your email>
2026-04-20 12:43:14 +0200

    backend/src/main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Étape cruciale : Activer les CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // L'URL de ton frontend Next.js
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // Écoute sur le port 3001
   await app.listen(process.env.PORT ?? 3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+});
